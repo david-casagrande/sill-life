7052f2ec56109738f55057039621ae9a11752488
David Casagrande <dncasagrande@gmail.com>
2015-02-07 22:22:53 -0500

    take advantage of dashicons for custom post types

--- functions/custom_post_types/classes.php
+++ functions/custom_post_types/classes.php
@@ -4,7 +4,7 @@
     'post_label' => 'Classes',
     'post_new_item_label' => 'Add a New Class',
     'post_slug' => '_classes',
-    //'post_menu_icon' => 'palette.png',
+    'menu_icon' => 'dashicons-welcome-learn-more',
     //'taxonomy_type' => 'artwork-category',
     //'taxonomy_label' => 'Artwork',
     //'taxonomy_slug' => 'artwork'
