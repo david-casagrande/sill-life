72df15b340e53a6adc4aa4c09c663760a8e71c02
David Casagrande <dncasagrande@gmail.com>
2015-02-08 00:14:50 -0500

    added js code to handle changing images in the terrarium gallery

--- templates/terrarium_gallery.php
+++ templates/terrarium_gallery.php
@@ -5,14 +5,14 @@ if(!isset($columns)) {
 if($gallery) {
   echo "<div class=\"{$columns} columns\">";
     $first_gallery_img = wp_get_attachment_image_src($gallery[0], 'full');
-    echo "<img src=\"{$first_gallery_img[0]}\" />";
+    echo "<img src=\"{$first_gallery_img[0]}\" class=\"gallery-main-image\"/>";
     echo "<ul class=\"small-block-grid-4\">";
     foreach ($gallery as $gallery_item) {
       $gallery_thumbnail = wp_get_attachment_image_src($gallery_item, 'thumbnail');
       $gallery_full = wp_get_attachment_image_src($gallery_item, 'full');
 
       echo "<li>";
-        echo "<a href=\"{$gallery_full[0]}\" target=\"_blank\">";
+        echo "<a href=\"{$gallery_full[0]}\" target=\"_blank\" onclick=\"SILL_LIFE.galleryClick(event)\">";
           echo "<img src=\"{$gallery_thumbnail[0]}\" class=\"no-padding-bottom\"/>";
         echo "</a>";
       echo "</li>";
