b2cfbfc6b6871dec195e94b2cba65e7067bde47f
David Casagrande <dncasagrande@gmail.com>
2015-02-18 23:02:24 -0500

    made main nav links bolder, reduced spacing on terrariums page, updated link color for narratives

--- javascript/sill-life.js
+++ javascript/sill-life.js
@@ -1,21 +1,41 @@
 (function() {
 
+  var galleryMainImage;
+
   function galleryClick(e) {
     e.preventDefault();
-    var imgSrc = e.target.parentNode.getAttribute('href'),
-        galleryMainImage = e.target.offsetParent.getElementsByClassName('gallery-main-image')[0];
+    var imgSrc = e.target.parentNode.getAttribute('href');
+
+    galleryMainImage = e.target.offsetParent.getElementsByClassName('gallery-main-image')[0];
 
     if(galleryMainImage.getAttribute('src') === imgSrc) { return; }
-
-    // console.log('change')
-    // var img = new Image(imgSrc);
-    // img.src = imgSrc;
-    // img.onload = function() {
-    //   console.log(this)
-    // }
-
+    //getFile(imgSrc);
     galleryMainImage.setAttribute('src', imgSrc);
   }
+
+  function getFile(file) {
+    var oReq = new XMLHttpRequest();
+    oReq.onload = function() {
+      console.log(this.response);
+      readFile(this.response);
+    };
+    oReq.onprogress = function(e) {
+      console.log(e)
+    };
+    oReq.open('get', file, true);
+    oReq.responseType = 'blob';
+    oReq.send();
+  }
+
+  function readFile(blob) {
+    var reader  = new FileReader();
+    reader.onloadend = function () {
+      console.log(reader.result);
+      galleryMainImage.setAttribute('src', reader.result);
+    }
+    reader.readAsDataURL(blob);
+  }
+
 
   window.SILL_LIFE = {
     galleryClick: galleryClick
--- templates/page.php
+++ templates/page.php
@@ -1,4 +1,5 @@
-<div class="narrative content-padding-side content-padding-top row bg-white">
+<?php if(!isset($page_class_names)) { $page_class_names = ''; } ?>
+<div class="narrative content-padding-side content-padding-top row bg-white <?php echo $page_class_names; ?>">
   <?php
     if ( have_posts() ) {
       while ( have_posts() ) { the_post(); ?>
