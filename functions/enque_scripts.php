8f7eed40b860074853eb451fe211cf823293bce4
David Casagrande <dncasagrande@gmail.com>
2015-02-21 22:37:35 -0500

    minify js for production

--- functions/enque_scripts.php
+++ functions/enque_scripts.php
@@ -1,8 +1,9 @@
 <?php
   function register_scripts() {
     $template_dir = get_template_directory_uri();
+    $js_file = (defined('PRODUCTION') && constant('PRODUCTION')) ? '/javascript/dist/sill-life.js' : '/javascript/sill-life.js';
 
-    wp_register_script('sill_life', $template_dir.'/javascript/sill-life.js', [], '0.0.1', true);
+    wp_register_script('sill_life', $template_dir.$js_file, [], '0.0.1', true);
     wp_enqueue_script('sill_life');
   }
 
--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp')
 var minifyCSS = require('gulp-minify-css')
 var rename = require("gulp-rename")
 var sass = require('gulp-sass')
+var uglify = require('gulp-uglify')
 //var sourcemaps = require('gulp-sourcemaps')
 
 var scssPath = ['./scss/**/*.scss', './bower_components/foundation/scss/**/*.scss']
@@ -22,8 +23,16 @@ gulp.task('minify-sass', ['sass'], function() {
     .pipe(gulp.dest('./css'))
 })
 
+gulp.task('minify-js', function() {
+  gulp.src('javascript/*.js')
+    .pipe(uglify({
+      mangle: true
+    }))
+    .pipe(gulp.dest('javascript/dist'))
+});
+
 gulp.task('watch', function() {
   gulp.watch(scssPath, ['sass', 'minify-sass'])
 })
 
-gulp.task('default', ['watch', 'sass', 'minify-sass'])
+gulp.task('default', ['watch', 'sass', 'minify-sass', 'minify-js'])
