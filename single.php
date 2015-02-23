928f9ccf0c974315f2724bd04abd51c6d0a7a888
David Casagrande <dncasagrande@gmail.com>
2015-02-23 14:07:02 -0500

    wrap terrarium template correctly

--- single.php
+++ single.php
@@ -6,7 +6,9 @@
       if ( have_posts() ) {
         while ( have_posts() ) { the_post();
           if(get_post_type() === 'terrariums') {
-            include('templates/terrarium.php');
+            echo "<div class=\"row content-padding-side narrative bg-white\">";
+              include('templates/terrarium.php');
+            echo "</div>";
           }
           elseif(get_post_type() === 'classes') {
             include('templates/classes.php');
