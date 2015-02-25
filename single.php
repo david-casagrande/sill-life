e9c8995c0d4f8a0d8cd2d3828359c4af87707541
David Casagrande <dncasagrande@gmail.com>
2015-02-24 20:49:36 -0500

    added some styles so single pages show correctly for terrariums and classes

--- single.php
+++ single.php
@@ -2,20 +2,20 @@
 
 <div class="content">
   <div class="row narrative">
-    <?php
-      if ( have_posts() ) {
-        while ( have_posts() ) { the_post();
-          if(get_post_type() === 'terrariums') {
-            echo "<div class=\"row content-padding-side narrative bg-white\">";
-              include('templates/terrarium.php');
-            echo "</div>";
-          }
-          elseif(get_post_type() === 'classes') {
-            include('templates/classes.php');
+    <div class="row content-padding-side narrative bg-white no-border-title">
+      <?php
+        if ( have_posts() ) {
+          while ( have_posts() ) { the_post();
+            if(get_post_type() === 'terrariums') {
+                include('templates/terrarium.php');
+            }
+            elseif(get_post_type() === 'classes') {
+              include('templates/classes.php');
+            }
           }
         }
-      }
-    ?>
+      ?>
+    </div>
   </div>
 </div>
 
