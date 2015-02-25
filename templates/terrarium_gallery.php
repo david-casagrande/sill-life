c315dde8553d8945dd42ca721696c5ca7f87a871
David Casagrande <dncasagrande@gmail.com>
2015-02-24 20:58:11 -0500

    added order now button

--- templates/terrarium_gallery.php
+++ templates/terrarium_gallery.php
@@ -3,9 +3,15 @@ if(!isset($columns)) {
   $columns = "large-12";
 }
 if($gallery) {
+  $order_url = get_field('order_url');
   echo "<div class=\"{$columns} columns\">";
     $first_gallery_img = wp_get_attachment_image_src($gallery[0], 'full');
-    echo "<img src=\"{$first_gallery_img[0]}\" class=\"gallery-main-image\"/>";
+    echo "<div class=\"gallery-main-image-wrapper\">";
+      echo "<img src=\"{$first_gallery_img[0]}\" class=\"gallery-main-image no-padding-bottom\"/>";
+      if($order_url) {
+        echo "<a href=\"{$order_url}\" target=\"_blank\" class=\"button order-terrarium\">ORDER</a>";
+      }
+    echo "</div>";
     echo "<ul class=\"small-block-grid-4\">";
     foreach ($gallery as $gallery_item) {
       $gallery_thumbnail = wp_get_attachment_image_src($gallery_item, 'thumbnail');
