c4fbb919aa7afa160dfa08ebde33bc879756b182
David Casagrande <dncasagrande@gmail.com>
2015-02-25 21:04:56 -0500

    made improvements to handling the layout of terrariums and classes page

--- page-terrariums.php
+++ page-terrariums.php
@@ -1,10 +1,8 @@
 <?php get_header(); ?>
 
 <div class="content">
-  <?php $page_class_names = 'no-padding-bottom'; ?>
-  <div class="terrariums-overview bg-white content-padding-bottom">
-    <?php include('templates/page.php'); ?>
-  </div>
+  <?php $page_class_names = 'content-padding-bottom'; ?>
+  <?php include('templates/page.php'); ?>
   <?php include('templates/all_terrariums.php'); ?>
 </div>
 
